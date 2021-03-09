f09f8c8dbb574694f3e96b655c251563b7a5a03f
senisioi <sergiu.nisioi@gmail.com>
2021-03-09 19:11:52 +0200

    upd scrpt

--- src/t1_router.sh
+++ src/t1_router.sh
@@ -10,10 +10,13 @@ iptables -t nat -A POSTROUTING -j MASQUERADE
 
 tc qdisc add dev eth0 root netem delay 100ms 10ms 25% loss 5% 25% corrupt 10% reorder 25% 50% && \
 
-echo "successfully set restrictions: "
-
+echo "restrictions have been set on eth0: " && \
 tc qdisc show dev eth0 && \
+echo "restrictions have been set on eth1: " && \
 tc qdisc show dev eth1 && \
 
-
 sleep infinity
+
+#remove restrictions with
+# tc qdisc del dev eth0 root || tc qdisc del dev eth1 root
+#
